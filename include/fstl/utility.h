e7e444988a0848f9a284e882687e1449598396d6
bmanga <bruno.manga95@gmail.com>
2019-01-16 20:59:29 +0000

    pair: add constructor

--- include/fstl/utility.h
+++ include/fstl/utility.h
@@ -6,6 +6,8 @@
 namespace fstl {
 template<class First, class Second>
 struct pair {
+  pair() = default;
+  pair(const First &fst, const Second &snd) : first(fst), second(snd) {}
   First first {};
   Second second {};
 };
